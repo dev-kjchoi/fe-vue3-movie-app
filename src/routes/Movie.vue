64b14e670d89127cfac433050019da142d5314bc
dev-kjchoi <kwangjiya@gmail.com>
2021-11-09 13:46:57 +0900

    Movie complete

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -21,7 +21,9 @@
           backgroundImage: `url(${requestDiffSizeImage(theMovie.Poster)})`,
         }"
         class="poster"
-      ></div>
+      >
+        <Loader v-if="imageLoading" absolute />
+      </div>
       <div class="specs">
         <div class="title">
           {{ theMovie.Title }}
@@ -76,6 +78,11 @@ export default {
   components: {
     Loader,
   },
+  data() {
+    return {
+      imageLoading: true,
+    };
+  },
   computed: {
     theMovie() {
       return this.$store.state.movie.theMovie;
@@ -85,14 +92,21 @@ export default {
     },
   },
   created() {
-    console.log(this.$route);
     this.$store.dispatch("movie/searchMovieWithId", {
       id: this.$route.params.id,
     });
   },
   methods: {
     requestDiffSizeImage(url, size = 700) {
-      return url.replace("SX300", `SX${size}`);
+      if (!url || url === "N/A") {
+        this.imageLoading = false;
+        return "";
+      }
+      const src = url.replace("SX300", `SX${size}`);
+      this.$loadImage(src).then(() => {
+        this.imageLoading = false;
+      });
+      return src;
     },
   },
 };
@@ -151,6 +165,7 @@ export default {
     background-color: $gray-200;
     background-size: cover;
     background-position: center;
+    position: relative;
   }
   .specs {
     flex-grow: 1;
