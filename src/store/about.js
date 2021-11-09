14ccdb4d51f7c9e5799a90b1abe51debfa817369
dev-kjchoi <kwangjiya@gmail.com>
2021-11-09 17:49:50 +0900

    apply bootstrap breakpoint and sass-loader

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: "KJCHOI",
+    email: "kwangjiya@gmail.com",
+    blog: "https://traderkj.com",
+    phone: "+82-10-1234-5678",
+    image: "https://heropy.blog/css/images/logo.png",
+  }),
+};
