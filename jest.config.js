ce2e13d2d0b9d7db346e197f3feee7ff642dfcab
dev-kjchoi <kwangjiya@gmail.com>
2021-11-12 17:31:42 +0900

    jest unit test

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  // 파일 확장자를 지정하지 않는 경우, Jest가 검색할 확장자 목록
+  // 일반적으로 많이 사용되는 모듈의 확장자를 지정
+  // 예: 'import HelloWorld from "~/components/HelloWorld";'
+  moduleFileExtensions: ["js", "vue"],
+
+  // '~' 같은 경로 별칭을 매핑
+  // '<rootDir>' 토큰을 사용해 루트 경로를 참조 할 수 있음
+  // 예: 'import HelloWorld from "~/components/HelloWorld";'
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/src/$1",
+  },
+
+  // 일치하는 경로에서는 모듈을 가져오지 않음
+  // '<rootDir>' 토큰을 사용해 루트 경로를 참조 할 수 있음
+  modulePathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist"],
+
+  // jsdom 환경에 대한 URL을 설정
+  // https://github.com/facebook/jest/issues/6766
+  testURL: "http://localhost",
+
+  // 정규식과 일치하는 파일의 변환 모듈을 지정
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\.js$": "babel-jest",
+  },
+};
---
+++ tests/components/Movie.test.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
+import Movie from "~/routes/Movie";
+
+describe("routes/Movie", () => {
+  let wrapper;
+
+  beforeEach(async () => {
+    wrapper = shallowMount(Movie);
+  });
+
+  test("", () => {});
+});
